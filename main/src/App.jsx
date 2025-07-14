772c7d91a319e5b6e48abb1b4e8a34d3cff2483a
Tess <tessmueske@gmail.com>
2025-07-14 13:16:40 +0100

    font change

---
+++ main/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to Speed Dating España!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
